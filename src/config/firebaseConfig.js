345d9f6861c0c325aef58c0197435fa2dd93ce24
Sara Mejía Jiménez <ingesara222@gmail.com>
2026-09-08 20:50:13 -0500

    adelanto: authcontex,authservices y modificación firebaseConfig

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,23 @@
+
 import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCEjptb1huxgnCMDQadM_EZ93XbAx0i_B4",
+  authDomain: "bolsilloaldia.firebaseapp.com",
+  projectId: "bolsilloaldia",
+  storageBucket: "bolsilloaldia.firebasestorage.app",
+  messagingSenderId: "303096486898",
+  appId: "1:303096486898:web:264e799e9c9ea500f6e96b",
+  measurementId: "G-SH3KK908WS"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const db = getFirestore(app);
